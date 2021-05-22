5983bb52f8a2e68016e88c623bc4729ad158a4e8
Hazal Uzar <39498419+hazaluzar@users.noreply.github.com>
2021-05-22 22:06:48 +0300

    Add files via upload

---
+++ frontend/src/views/QueSelectClub.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <SubClubSelectionHeader/>
+  </div>
+  <div class="page">
+    <h1 class=title>What are your interests?</h1>
+    <p>You can find sub-clubs by clicking on the categories in the menu. Please select at least one sub-club according to your interests. After you finish your selection, you will be applied the questionnaire based on the sub-clubs you chose.</p>
+    <div>  
+      <SubClubMenu/>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+// @ is an alias to /src
+import SubClubMenu from '@/components/SubClubMenu.vue'
+import SubClubSelectionHeader from '@/components/SubClubSelectionHeader.vue'
+
+export default {
+  name: 'QueSelectClub',
+  components: {
+    SubClubMenu,
+    SubClubSelectionHeader
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Alegreya+Sans:ital,wght@0,400;0,500;0,700;0,800;1,400;1,500;1,700&display=swap');
+    :root{
+    --primary-color: #BA7EA0;
+  }
+
+  .page{
+    padding:10%;
+    padding-top:0;
+  }
+  .title{
+    font-family: 'Alegreya Sans', sans-serif;
+    color:var(--primary-color);
+    font-size: 2em;
+    padding: 30px;
+    padding-left: 50px;
+    margin:0;
+  }
+  p{
+    font-family: 'Alegreya Sans', sans-serif;
+    font-size: 1.5em;
+    font-weight: 500;
+    margin:0;
+    padding-bottom: 20px;
+  }
+
+</style>
