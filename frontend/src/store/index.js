135e34a050f1d7a9b82b1b9d444ebaf4cf050cb3
b21827282 <44412189+b21827282@users.noreply.github.com>
2021-05-23 21:00:06 +0300

    Add files via upload

---
+++ frontend/src/store/index.js
@@ -0,0 +1,172 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    categories: [
+      {name:'Sports', sub_clubs:[
+          {name: 'Basketball'},
+          {name: 'Football'},
+          {name: 'Yoga'},
+          {name: 'Tennis'},
+          {name: 'Swimming'},
+      ]},
+      {name:'Music', sub_clubs:[
+          {name: 'Violin'},
+          {name: 'Piano'},
+          {name: 'Guitar'},
+          {name: 'Rock Music'},
+      ]},
+      {name:'Visual Arts', sub_clubs:[
+          {name: 'Painting'},
+          {name: 'Drawing'},
+          {name: 'Ceramics'},
+          {name: 'Photography'},
+      ]},
+      {name:'Hobbies', sub_clubs:[
+          {name: 'Puzzle'},
+          {name: 'Sudoku'},
+          {name: 'Reading'},
+          {name: 'Cooking'},
+          {name: 'Chess'},
+      ]}
+    ],
+    active: [],
+    selected_sub_clubs: [],
+    new_sub_clubs: [{name: 'Electronic Music'},{name: 'Sculpture'}],
+
+    // current user
+    user:{
+        name: "user1",
+        password: "123456",
+        role: "member",
+        subclubs:[{ 
+            id:1,
+            name:"Yoga",
+            img: "../assets/sub-clubs-images/Yoga.jpg",
+            rate: 3.2,
+            about:"This is about yoga subclub"
+        },
+        {
+            id:2,
+            name: "Football",
+            img: "../assets/sub-clubs-images/Piano.jpg",
+            rate:4.0,
+            about:"This is about football subclub"
+        }]
+    },
+
+    // every subclub
+    subclubs:[{
+        id:0,
+        name:"Yoga",
+        img:"../assets/sub-clubs-images/Yoga.jpg",
+        rate: 3.2,
+        about:"This is about yoga subclub",
+        rates_reviews:[{
+                rate: 3,
+                review: "not bad",
+                },
+                {
+                rate: 5,
+                review: "very good",
+                },
+                {
+                rate: 5,
+                review: "very good",
+                },
+                {
+                rate: 5,
+                review: "very good",
+                },
+                {
+                rate: 5,
+                review: "very good",
+                },
+                {
+                rate: 5,
+                review: "very good",
+                },
+                {
+                rate: 5,
+                review: "very good",
+                },
+            ]
+    },{
+        id:0,
+        name:"Piano",
+        img:"../assets/sub-clubs-images/Piano.jpg",
+        rate: 3.2,
+        about:"This is about piano subclub",
+        rates_reviews:[{
+                rate: 3,
+                review: "not bad",
+                },
+                {
+                rate: 5,
+                review: "i like this subclub",
+                },
+                {
+                rate: 5,
+                review: "very good",
+                },
+                {
+                rate: 5,
+                review: "very good",
+                },
+                {
+                rate: 5,
+                review: "very good",
+                },
+                {
+                rate: 5,
+                review: "very good",
+                },
+                {
+                rate: 5,
+                review: "very good",
+                },
+            ]
+    },
+
+        
+
+    ]
+
+    
+
+
+
+  },
+  mutations: {
+    makeActive(state, category){
+      const index = state.active.indexOf(category)
+      if (index >= 0)
+          state.active.splice(index,1)
+      else
+          state.active.push(category)
+    },
+    addSubClub(state,sub_club){
+      const index = state.selected_sub_clubs.indexOf(sub_club)
+      if (index < 0)
+          state.selected_sub_clubs.push(sub_club)
+    },
+    removeSubClub(state, sub_club){
+      const index = state.selected_sub_clubs.indexOf(sub_club)
+      if (index >= 0)
+          state.selected_sub_clubs.splice(index,1)
+    },
+
+  },
+  actions: {
+
+  },
+  modules: {
+  },
+
+  getters:{
+
+    getUser(state) {
+      return state.user
+    }
+  }
+})
