023874beb1a4d2311ef0bd89a8bc4cee768410c0
b21827282 <44412189+b21827282@users.noreply.github.com>
2021-05-26 17:17:41 +0300

    Update index.js

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -36,32 +36,38 @@ export default createStore({
 
     //current user
     user:{ 
-      id:1,
-      username:"bandit",
+      id:0,
+      firstName:"user1",
       email:"basak945@gmail.com",
       subclubs:[{ 
           id:1,
-          name:"Yoga",
-          img: require("../assets/sub-clubs-images/Yoga.jpeg"),
-          rate: 3.2,
+          title:"Yoga",
+          imgUrl: require("../assets/sub-clubs-images/Yoga.jpeg"),
+          averageRate: 3.2,
           about:"This is about yoga subclub"
         },
       {
           id:2,
-          name: "Piano",
-          img:require("../assets/sub-clubs-images/Piano.jpeg"),
-          rate:4.0,
+          title: "Piano",
+          imgUrl:require("../assets/sub-clubs-images/Piano.jpeg"),
+          averageRate:4.0,
           about:"This is about piano subclub"
-      }]
+      },{ 
+        id:3,
+        title:"Puzzle",
+        imgUrl: require("../assets/sub-clubs-images/Puzzle.jpeg"),
+        averageRate: 3.2,
+        about:"This is about puzzle subclub"
+      },]
  
     },
     
     //all sub-clubs
     subclubs:[{
-      id:0,
-      name:"Yoga",
-      img: require("../assets/sub-clubs-images/Yoga.jpeg"),
-      rate: 3.2,
+      id:1,
+      title:"Yoga",
+      imgUrl: "../assets/sub-clubs-images/Yoga.jpg",
+      averageRate: 3.2,
       about:"This is about yoga subclub",
       rates_reviews:[{
               rate: 3,
@@ -84,10 +90,10 @@ export default createStore({
                   review: "very good",     
                   },]
       },{
-      id:1,
-      name:"Piano",
-      img: require("../assets/sub-clubs-images/Piano.jpeg"),
-      rate: 4,
+      id:2,
+      title:"Piano",
+      imgUrl: "../assets/sub-clubs-images/Piano.jpg",
+      averageRate: 4,
       about:"This is about Piano subclub",
       rates_reviews:[{
               rate: 3,
@@ -98,6 +104,22 @@ export default createStore({
               review: "very good",
               },
           ]}, 
+      { 
+        id:3,
+        title:"Puzzle",
+        imgUrl: "../assets/sub-clubs-images/Puzzle.jpeg",
+        averageRate: 3.2,
+        about:"This is about puzzle subclub",
+        rates_reviews:[{
+          rate: 3,
+          review: "not bad",
+          },
+          {
+          rate: 5,
+          review: "very good",
+          },
+      ]
+      },
     ],
     
     
@@ -120,7 +142,7 @@ export default createStore({
       const index = state.selected_sub_clubs.indexOf(sub_club)
       if (index >= 0)
           state.selected_sub_clubs.splice(index,1)
-    }
+    },
   },
   actions: {
    
@@ -129,10 +151,16 @@ export default createStore({
   },
   getters:{
     getSubClubs(state){
-      return state.subclubs.filter(s => s.name)
+      return state.subclubs.filter(s => s.title)
+    },
+
+    getUserSubClubs(state){
+      return state.user.subclubs.filter(s => s.title)
+    },
+
+    getUserFirstName(state){
+      return state.user.firstName
     }
   }
-
-
 
 })
